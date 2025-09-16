8eec184dd0fe6340941fb0e43bad9eb2e1e30d0a
Fred <huertaserick@yahoo.ie>
2025-09-16 19:58:35 +0100

    Wordpress code

--- app/public/experiment.php
+++ app/public/experiment.php
@@ -1,2 +1,7 @@
-<h1>This page is all about Fred</h1>
-<h2>This page is all about me! Leggo</h2>
+<?php echo 2 + 2;
+$myname = "John Doe";
+?>
+
+<h1>This page is all about <?= $myname ?></h1>
+<?php echo 5 * 5 ?>
+<h2>This page is all about <?php echo $myname ?>! Leggo</h2>
