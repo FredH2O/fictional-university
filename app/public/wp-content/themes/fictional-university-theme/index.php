5739b29b7fd1a34b32b8e1fdd181bb1c30b38440
Fred <huertaserick@yahoo.ie>
2025-09-17 13:37:09 +0100

    Functions in php

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,21 @@
-This is our completely amazing custom theme.
+<?php
+
+function greet($name, $colour)
+{
+    echo "<p>Hi, my name is $name and my favourite colour is $colour</p>";
+}
+
+greet("John", "Red");
+greet("Jerry", "Yellow");
+?>
+
+<h1>
+    <?php
+    bloginfo("name",)
+    ?>
+</h1>
+<p>
+    <?php
+    bloginfo("description",)
+    ?>
+</p>
