8a4fbe15a4718c14c3190eacdabdb03e7ba79d66
Fred <huertaserick@yahoo.ie>
2025-09-29 21:18:03 +0100

    arrays and while loop

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
-function greet($name, $color){
-    echo "<p> Hi, my name is $name and my fave colour is $color </p>";
+$names = array("Brad", "Pitt", "John");
+
+$count = 0;
+
+while ($count < count($names)) {
+    echo "<li>Hi, my name is $names[$count]</li>";
+    $count++;
 }
 
-greet("John", "Red");
-greet("Jane", "Black");
 
 ?>
-
-<h1> <?php bloginfo("name"); ?> </h1>
-<p><?php bloginfo("description")?></p>
