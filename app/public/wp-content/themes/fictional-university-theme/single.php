38cc256bafb4b65683991e9298a074740a965a52
Fred <huertaserick@yahoo.ie>
2025-09-29 21:48:17 +0100

    Learned about the_post() , creating single.php, page.php and more

--- app/public/wp-content/themes/fictional-university-theme/single.php
+++ app/public/wp-content/themes/fictional-university-theme/single.php
@@ -3,7 +3,7 @@
 while (have_posts()) {
     the_post(); ?>
     <h2>
-        <a href="<?php the_permalink() ?> "> <?php the_title() ?> </a>
+        <?php the_title() ?>
     </h2>
     <p> <?php the_content() ?> </p>
     <hr>
