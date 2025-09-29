ab33de5e577522ea75cbb6a47e88d39a5c106443
Fred <huertaserick@yahoo.ie>
2025-09-29 20:02:20 +0100

    Learning PHP, using echo, using built in functions and outputting data?

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,13 @@
-This is our completely amazing custom theme
+<?php 
+
+function greet($name, $color){
+    echo "<p> Hi, my name is $name and my fave colour is $color </p>";
+}
+
+greet("John", "Red");
+greet("Jane", "Black");
+
+?>
+
+<h1> <?php bloginfo("name"); ?> </h1>
+<p><?php bloginfo("description")?></p>
