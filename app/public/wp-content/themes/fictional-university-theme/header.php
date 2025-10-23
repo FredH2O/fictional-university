9d0227c7d54edcc7898492da4e21e6226a7a7f9e
Fred <huertaserick@yahoo.ie>
2025-10-23 21:44:30 +0100

    Php-lesson-16 || or and && , type checking

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -28,7 +28,7 @@
                             <a href="#">Programs</a>
                         </li>
 
-                        <li <?php if (get_post_type() == "event") echo 'class="current-menu-item"'; ?>>
+                        <li <?php if (get_post_type() == "event" || is_page("past-events")) echo 'class="current-menu-item"'; ?>>
                             <a href="<?php echo get_post_type_archive_link("event"); ?>">
                                 Events
                             </a>
